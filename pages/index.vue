793bab25643d432678f0737f7cd85003b3b9a711
blryface <eduardotenoriopo+nugit@gmail.com>
2024-05-25 00:15:53 -0300

    remove the stuff i wrote while.. mentally unwell ig

--- pages/index.vue
+++ pages/index.vue
@@ -14,19 +14,7 @@
         <ProjectParagraph>
 
             Essential is not really the best mod for many reasons, such as breaking with a lot of mods, being closed source, and shipping an Installer as the mod download. </ProjectParagraph>
-<!--
-        <ProjectParagraph>
 
-            There are many alternatives that can do what essential does, for free, and open source. Some features are exclusive to essential, but people may be working on new alternatives. 
-
-        </ProjectParagraph>
-
-        <ProjectParagraph>
-
-            Essential brings the good and bad of bedrock to Java: Easy multiplayer, Group Chat system (Realms Plus), paid cosmetics, and breaking easily.
-
-        </ProjectParagraph>
-            idfk what else to write, and this kinda feels... off -->
     </projectContainer>
 
 </template>
