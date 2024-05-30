e055763e0a39e3256936830e918c371229c93893
null <null>
2024-05-30 17:14:53 -0300

    Add [ugly] github link

--- components/pageFooter.vue
+++ components/pageFooter.vue
@@ -1,5 +1,6 @@
 <template>
 
     <span class="font-light text-white text-center text-xs text-center"> THANKS FOR READING - WRITTEN BY BLURRYFACE - WEBSITE BY WORLDWIDEPIXEL AND BLURRYFACE </span>
+    <NuxtLink to="https://github.com/blryface/notessential"> <user> Website Source <LucideGithub /> </user> </NuxtLink> <!-- THIS IS VERY TEMPORARY, IM GONNA MAKE PIXEL MAKE IT BETTER SOON™ -->
 
 </template>
