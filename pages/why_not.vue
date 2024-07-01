2bf1f5edb4070fa32d679799d6ae400e5343431e
KTrain <69028025+KTrain5169@users.noreply.github.com>
2024-07-01 21:12:14 +1000

    add: Why not? page
    Working on #12

---
+++ pages/why_not.vue
@@ -0,0 +1,31 @@
+<template>
+    <projectContainer class="flex gap-8">
+
+        <ProjectTitleBar>
+
+            <span class="text-[1.5rem] sm:text-[2rem] md:text-[3rem] lg:text-[4rem] internalFont flex flex-row flex-wrap gap-3">
+                <span class="font-bold"> WHY SHOULD YOU NOT USE </span
+                    class="brand-gradient">ESSENTIAL?</span>.</span>
+            </span>
+
+        </ProjectTitleBar>
+
+        <ProjectParagraph>
+            Why are we not recommending Essential? This page will list our reasons why you should use alternatives.
+        </ProjectParagraph>
+
+    </projectContainer>
+
+</template>
+<!-- ⬇️ is supposed to -->
+<script setup lang="ts">
+useSeoMeta({
+    title: 'Why? | Not Essential',
+    description: "Essential has many issues. This website tells why, and provides alternatives.",
+    ogImage: "/img/icon512.png",
+})
+</script>
+
+<!--
+:3
+-->
