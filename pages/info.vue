e3369605181c81287dda2deb0e83f564fc112d53
blryface <124838505+blryface@users.noreply.github.com>
2024-07-01 08:31:00 -0300

    i decided to base it off index

---
+++ pages/info.vue
@@ -0,0 +1,35 @@
+<template>
+
+        <projectContainer class="flex gap-8">
+
+                <ProjectTitleBar>
+
+                            <span class="text-[1.5rem] sm:text-[2rem] md:text-[3rem] lg:text-[4rem] internalFont flex flex-row flex-wrap gap-3">
+                                            <span class="font-bold"> THERE ARE BETTER WAYS TO PLAY <span
+                                                                    class="brand-gradient">MINECRAFT</span>.</span>
+                                                                                </span>
+
+                                                                                        </ProjectTitleBar>
+
+                                                                                                <ProjectParagraph>
+                                                                                                            Don't be forced to use Essential. There are countless open-source alternatives to help you.
+                                                                                                                    </ProjectParagraph>
+
+                                                                                                                            <NuxtLink to="/alternatives"> <BigButton> <LucideArrowUpRight /> ALTERNATIVES </BigButton> </NuxtLink>
+
+
+                                                                                                                                </projectContainer>
+
+                                                                                                                                </template>
+                                                                                                                                <!-- ⬇️ is supposed to -->
+                                                                                                                                <script setup lang="ts">
+                                                                                                                                useSeoMeta({
+                                                                                                                                    title: 'Home | Not Essential',
+                                                                                                                                        description: "Essential has many issues. This website tells why, and provides alternatives.",
+                                                                                                                                            ogImage: "/img/icon512.png",
+                                                                                                                                            })
+                                                                                                                                            </script>
+
+                                                                                                                                            <!--
+                                                                                                                                            :3
+                                                                                                                                            -->
