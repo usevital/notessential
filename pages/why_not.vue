8129ef243a365c0b0a8728eec8d8c025e57d4879
KTrain <69028025+KTrain5169@users.noreply.github.com>
2024-07-01 21:21:54 +1000

    fix: minor issue & word usage...

--- pages/why_not.vue
+++ pages/why_not.vue
@@ -1,18 +1,21 @@
 <template>
+
     <projectContainer class="flex gap-8">
 
         <ProjectTitleBar>
 
             <span class="text-[1.5rem] sm:text-[2rem] md:text-[3rem] lg:text-[4rem] internalFont flex flex-row flex-wrap gap-3">
                 <span class="font-bold"> WHY SHOULD YOU NOT USE </span
-                    class="brand-gradient">ESSENTIAL?</span>.</span>
+                        class="brand-gradient">ESSENTIAL?</span>.</span>
             </span>
 
         </ProjectTitleBar>
 
         <ProjectParagraph>
-            Why are we not recommending Essential? This page will list our reasons why you should use alternatives.
+            Why are we recommending not to use Essential? This page will list our reasons why you should use alternatives.
         </ProjectParagraph>
+
+        <NuxtLink to="/alternatives"> <BigButton> <LucideArrowUpRight /> ALTERNATIVES </BigButton> </NuxtLink>
 
     </projectContainer>
 
