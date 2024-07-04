02bfb216ab6211b3aaf19843b51cc2e5f8f4853f
KTrain <69028025+KTrain5169@users.noreply.github.com>
2024-07-04 15:00:56 +1000

    change: question icon

--- pages/index.vue
+++ pages/index.vue
@@ -21,7 +21,7 @@
     </NuxtLink>
 
     <NuxtLink class="w-fit" to="/info">
-      <BigButton> <LucideCircleHelp /> WHY NOT? </BigButton>
+      <BigButton> <LucideFileQuestion /> WHY NOT? </BigButton>
     </NuxtLink>
   </projectContainer>
 </template>
