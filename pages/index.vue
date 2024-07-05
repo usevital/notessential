6bdd95c8b2176ea35e203486868832c99a8a543e
KTrain <69028025+KTrain5169@users.noreply.github.com>
2024-07-05 21:21:08 +1000

    fix: remove centering buttons on sm devices

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,7 @@
       alternatives to help you.
     </ProjectParagraph>
 
-    <div class="button-container flex flex-row gap-4 sm:flex-col sm:items-center">
+    <div class="button-container flex flex-row gap-4 sm:flex-col">
       <NuxtLink class="w-fit" to="/alternatives">
         <BigButton> <LucideArrowUpRight /> ALTERNATIVES </BigButton>
       </NuxtLink>
