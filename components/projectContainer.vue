7d5cc222838f693d042d4c7eda9cdd4ad782f1e7
Nitrrine <nitrrine@team.lodine.xyz>
2024-07-06 13:01:00 +0500

    Add ProjectCard component

--- components/projectContainer.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div
-    class="flex flex-col gap-2 bg-[color:var(--brand-colour)] p-4 rounded-2xl"
-  >
-    <slot />
-  </div>
-</template>
--- components/projectParagraph.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="text-2xl">
-    <slot />
-  </div>
-</template>
