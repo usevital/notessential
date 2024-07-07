6e45c2a617989ddc576562b8a6282d41dac275b9
Nitrrine <43351072+Nitrrine@users.noreply.github.com>
2024-07-07 11:15:35 +0500

    Add ExternalLink component (#26)

--- components/pageFooter.vue
+++ components/pageFooter.vue
@@ -3,10 +3,10 @@
     <span class="font-[500] text-[var(--text-colour)] text-xs text-center">
       THANKS FOR READING - WRITTEN BY BLURRYFACE - DESIGNED BY WORLDWIDEPIXEL
       &bull;
-      <a class="underline" href="https://github.com/blryface/notessential">
-        PAGE SOURCE
-      </a></span
-    >
+      <ExternalLink to="https://github.com/blryface/notessential"
+        >PAGE SOURCE</ExternalLink
+      >
+    </span>
     <span class="font-[500] italic text-slate-400 text-xs text-center">
       THIS SITE IS IN NO WAY AFFILIATED WITH ESSENTIAL OR SPARK UNIVERSE
     </span>
