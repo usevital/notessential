7c819b6cab38d5d79aa59389e2cabb9cd38b86d2
Nitrrine <nitrrine@team.lodine.xyz>
2024-07-07 11:14:27 +0500

    Add ExternalLink component

---
+++ components/externalLink.vue
@@ -0,0 +1,18 @@
+<template>
+  <a
+    :href="to"
+    target="_blank"
+    rel="noopener noreferrer"
+    class="inline-flex flex-row items-center gap-1"
+  >
+    <LucideExternalLink v-if="showIcon" />
+    <slot />
+  </a>
+</template>
+
+<script setup>
+const props = defineProps({
+  to: String,
+  showIcon: Boolean,
+})
+</script>
