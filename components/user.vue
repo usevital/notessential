99d9719b507ceb242f80ccaa9948230ec748c8dd
Nitrrine <nitrrine@team.lodine.xyz>
2024-07-11 13:54:07 +0500

    Mark component User for deletion

--- components/user.vue
+++ components/user.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- ! DEPRECATED COMPONENT, MARKED FOR DELETION -->
   <span
     class="gap-4 flex flex-row font-bold text-2xl bg-[color:var(--brand-colour)] items-center p-4 rounded-2xl select-none duration-[0.4s] active:scale-[0.9]"
   >
