b7746a007530c9212e501e49cf73880b8eb8e400
KTrain5369 <69028025+KTrain5169@users.noreply.github.com>
2024-07-17 20:43:42 +1000

    chore: let me sleep prettier

--- pages/flags.vue
+++ pages/flags.vue
@@ -3,12 +3,15 @@
     <span
       class="text-[1.5rem] sm:text-[2rem] md:text-[3rem] lg:text-[4rem] internalFont flex flex-row flex-wrap gap-3"
     >
-      <span class="font-bold"> UH-OH! YOU REACHED <span class="brand-gradient">THE OLD EXPERIMENTS PAGE</span>!</span>
-    
+      <span class="font-bold">
+        UH-OH! YOU REACHED
+        <span class="brand-gradient">THE OLD EXPERIMENTS PAGE</span>!</span
+      >
     </span>
   </div>
 
   <p class="text-2xl">
-      You landed on the page we used to use for <a href="/experiments">experiments.</a>
+    You landed on the page we used to use for
+    <a href="/experiments">experiments.</a>
   </p>
 </template>
