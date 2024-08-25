1f4fff67eb544b7341d0393a86bad44165efe26f
KTrain5369 <69028025+KTrain5169@users.noreply.github.com>
2024-08-25 21:20:38 +1000

    add: reasons for why not essential

--- pages/index.vue
+++ pages/index.vue
@@ -60,20 +60,45 @@ const reasonsWhy = [
     ],
   },
   {
-    title: "Cluttered Interface",
+    title: "Data corruption",
     body: [
-      "Essential's interface has many options and buttons. These tend to lead to severe clutter in certain parts of the game.",
-      "Staying on the default configuration, the amount of screen space taken up by Essential makes the game cumbersome to navigate.",
+      "Following on from the previous point, their large modifications to the game can cause data loss or world corruption.",
+      "While, again, their decision to become source-available can help explain what's going on, it'd be better if these modifications weren't included in the first place.",
     ],
   },
   {
     title: "Security and Privacy",
-    body: [],
+    body: [
+      "Essential requires that you accept the ToS in order to use the mod. This can come across as a red flag to some, as generally this is only present as a confirmation that you don't mind your data being harvested.",
+      "While Essential's Privacy Policy states the data they collect (and it's not very significant to most people), most people will not be reading the policy, especially with the required legalese to cover their bases.",
+      "Additionally, this is a game played by kids, the cohort that are most likely clueless on what a Terms of Service or Privacy Policy is, and don't understand the data they are giving out by agreeing and especially don't understand the language being used.",
+    ],
   },
   {
     title: "Microtransactions",
-    body: [],
+    body: [
+      'In a game that is primarily played by children, Essential advertises its cosmetics shop, featuring microtransactions where you can buy "Essential Coins".',
+      "While Essential does need money to support their devs and some of their features, a store with microtransactions seems very predatory considering the context.",
+      'Additionally, they have a system where you can purchase coins only in "bundles" which encourages over-purchase of coins, potentially wasting the remaining coins.',
+      "This isn't exclusive to Essential, as it's a problem for Minecraft Bedrock Edition, as well as other (mainly mobile/gacha) games. This, however, makes it seem like they know what they are doing.",
+    ],
   },
+  {
+    title: "Cluttered Interface",
+    body: [
+      "Essential's interface has many options and buttons. These tend to lead to severe clutter in certain parts of the game.",
+      "Staying on the default configuration, the amount of screen space taken up by Essential makes the game cumbersome to navigate.",
+      "Additionally, some of the buttons on the main menu aren't necessary or can be placed better. It's not a lot, but on the default screen size the space it takes up can be significant.",
+    ],
+  },
+  {
+    title: "Too much bloat",
+    body: [
+      'Lastly, for a mod that advertises itself as the "essential tool for Minecraft", it has too many things included.',
+      "The concept of an all-in-one mod isn't bad, but as a mod that should easily fit with other mods, having too many features causes too many potential conflicts.",
+      "The problems mentioned previously regarding compatibility with other mods certainly doesn't help Essential's case either.",
+    ]
+  }
 ]
 
 useSeoMeta({
