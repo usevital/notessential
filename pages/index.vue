c7807ea488d1dafba3867348f3606e86475911a7
KTrain5369 <69028025+KTrain5169@users.noreply.github.com>
2024-09-26 15:19:30 +0700

    Finalise text in reasonsWhy, i18n support coming later

--- pages/index.vue
+++ pages/index.vue
@@ -67,6 +67,8 @@ const reasonsWhy = [
     body: [
       "Essential's large-sweeping modifications to the game cause many side effects, mainly incompatibility with other mods.",
       "While Essential's desision to go source-available (not the same as open-source) may help improve compatibility issues, it's too soon to tell.",
+      "For uninitiated users, open-source differs from source-available in that any user can send a request to fix an issue or add a new feature, which source-available doesn't let you do.",
+      "We have a gallery of people saying that removing Essential has fixed an issue they experienced, you can find this at https://notessentialsite.github.io/gallery/"
     ],
   },
   {
@@ -80,9 +82,9 @@ const reasonsWhy = [
     title: "Microtransactions",
     body: [
       'In a game that is primarily played by children, Essential advertises its cosmetics shop, featuring microtransactions where you can buy "Essential Coins".',
-      "While Essential does need money to support their devs and some of their features, a store with microtransactions seems very predatory considering the context.",
+      "While Essential does need money to support their devs and some of their features, a store with special currency microtransactions seems very predatory considering the context.",
       'Additionally, they have a system where you can purchase coins only in "bundles" which encourages over-purchase of coins, potentially wasting the remaining coins.',
-      "This isn't exclusive to Essential, as it's a problem for Minecraft Bedrock Edition, as well as other (mainly mobile/gacha) games. This, however, makes it seem like they know what they are doing.",
+      "This isn't exclusive to Essential, as it's a problem for Minecraft Bedrock Edition (which is a major annoyance point for most players), as well as other (mainly mobile/gacha) games. This, however, makes it seem like they know what they are doing.",
     ],
   },
   {
@@ -91,14 +93,16 @@ const reasonsWhy = [
       "Essential requires that you accept the ToS in order to use the mod. This can come across as a red flag to some, as generally this is only present as a confirmation that you don't mind your data being harvested.",
       "While Essential's Privacy Policy states the data they collect (and it's not very significant to most people), most people will not be reading the policy, especially with the required legalese to cover their bases.",
       "Additionally, this is a game played by kids, the cohort that are most likely clueless on what a Terms of Service or Privacy Policy is, and don't understand the data they are giving out by agreeing and especially don't understand the language being used, even if kids are legally not allowed to agree to contracts in some areas.",
-      "For full clarity, this is what Essential says (verbatim) they collect on their Privacy Policy (updated 27/8/2024): Minecraft UUID, Minecraft & modloader versions, Mod Checksums using MD5, User interfaces the player enters during their session, Cosmetics that players choose to preview, Essential config values (with random A/B testing), Servers you join (if you choose to enable it in privacy settings), and computer specs.",
+      "For full clarity, Essential says, on their Privacy Policy (checked 27/8/2024), they collect: Minecraft UUID, Minecraft & modloader versions, Mod Checksums using MD5, User interfaces the player enters during their session, Cosmetics that players choose to preview, Essential config values (with random A/B testing), Servers you join (if you choose to enable it in privacy settings), and computer specs.",
+      "Their privacy policy can be found at https://essential.gg/privacy-policy, which may be updated at any point after the writing of this.",
     ],
   },
   {
     title: "Too much bloat",
     body: [
-      'For a mod that advertises itself as the "essential tool for Minecraft", it has too many things included.',
+      'For a mod that advertises itself as the "essential tool for Minecraft", it has too many features included within.',
       "The concept of an all-in-one mod isn't bad, but as a mod that should easily fit with other mods, having too many features causes too many potential conflicts.",
+      "Being an all-in-one mod has downsides too, most notably that if a feature is crashing, the only easy solution for most users is removing the entire mod.",
       "The problems mentioned previously regarding compatibility with other mods certainly doesn't help Essential's case either.",
     ],
   },
