06de34d0b447deeb46c47b8effbd8943a492aacb
KTrain <69028025+KTrain5169@users.noreply.github.com>
2024-11-28 17:36:36 +1000

    migrating i18n to nuxtjs/i18n v9 because it breaks stuff ig

--- i18n/i18n.config.ts
+++ i18n/i18n.config.ts
@@ -1,23 +1,23 @@
 //If you take this out of alphabetical order I will eat you
 
-import arSA from "./lang/ar-SA.json"
-import deDE from "./lang/de-DE.json"
-import enPT from "./lang/en-PT.json"
-import enUD from "./lang/en-UD.json"
-import enUS from "./lang/en-US.json"
-import esES from "./lang/es-ES.json"
-import huHU from "./lang/hu-HU.json"
-import jaJP from "./lang/ja-JP.json"
-import lolUS from "./lang/lol-US.json"
-import noNO from "./lang/no-NO.json"
-import plPL from "./lang/pl-PL.json"
-import ptBR from "./lang/pt-BR.json"
-import ptPT from "./lang/pt-PT.json"
-import ruRU from "./lang/ru-RU.json"
-import ukUA from "./lang/uk-UA.json"
-import viVN from "./lang/vi-VN.json"
-import zhCN from "./lang/zh-CN.json"
-import zhTW from "./lang/zh-TW.json"
+import arSA from "./locales/ar-SA.json";
+import deDE from "./locales/de-DE.json";
+import enPT from "./locales/en-PT.json";
+import enUD from "./locales/en-UD.json";
+import enUS from "./locales/en-US.json";
+import esES from "./locales/es-ES.json";
+import huHU from "./locales/hu-HU.json";
+import jaJP from "./locales/ja-JP.json";
+import lolUS from "./locales/lol-US.json";
+import noNO from "./locales/no-NO.json";
+import plPL from "./locales/pl-PL.json";
+import ptBR from "./locales/pt-BR.json";
+import ptPT from "./locales/pt-PT.json";
+import ruRU from "./locales/ru-RU.json";
+import ukUA from "./locales/uk-UA.json";
+import viVN from "./locales/vi-VN.json";
+import zhCN from "./locales/zh-CN.json";
+import zhTW from "./locales/zh-TW.json";
 
 export default defineI18nConfig(() => ({
   legacy: false,
@@ -44,4 +44,4 @@ export default defineI18nConfig(() => ({
     "zh-CN": zhCN,
     "zh-TW": zhTW,
   },
-}))
+}));
