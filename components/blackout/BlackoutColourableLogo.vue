f426afc3b3ebfd731b7763efaab53d387a42002a
worldwidepixel <worldwidepxl@gmail.com>
2025-03-02 17:37:43 -0800

    By request of Blurry.

---
+++ components/blackout/BlackoutColourableLogo.vue
@@ -0,0 +1,39 @@
+<template>
+  <svg
+    viewBox="0 0 25 25"
+    version="1.1"
+    id="logotype"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:svg="http://www.w3.org/2000/svg"
+    :fill="color"
+  >
+    <defs id="defslogotype" />
+    <g id="logotypelayer" transform="translate(112.00347,-77.240964)">
+      <path
+        id="rect9248"
+        style="
+          font-variation-settings: normal;
+          fill-opacity: 1;
+          fill-rule: evenodd;
+          stroke: #000080;
+          stroke-width: 0;
+          stroke-linecap: round;
+          stroke-linejoin: round;
+          stroke-miterlimit: 4;
+          stroke-dasharray: none;
+          stroke-dashoffset: 0;
+          stroke-opacity: 1;
+          -inkscape-stroke: none;
+          stop-color: #000000;
+        "
+        d="m -112.00347,77.240965 v 9.9999 h 5.00022 v 5.000202 h -5.00022 v 9.999923 h 9.9999 v -5.000224 h 5.000224 v 5.000224 h 9.9999 v -9.999923 h -5.000201 v -5.000202 h 5.000201 v -9.9999 h -9.9999 v 5.000201 h -5.000224 v -5.000201 z"
+      />
+    </g>
+  </svg>
+</template>
+
+<script setup>
+const props = defineProps({
+  color: String,
+})
+</script>
