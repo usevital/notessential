22f3f6048a5dcabaee0723d53bbfa9e1f4c585f4
Nitrrine <nitrrine@team.lodine.xyz>
2025-03-08 21:54:03 +0500

    Update hero and do_not_harass translation keys

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,36 +8,115 @@
       class="sm:px-[calc(5%+2rem)] text-[color:var(--big-text-colour)] flex flex-col items-center select-none font-bold p-8 border-b border-b-slate-800"
     >
       <div
-        class="flex flex-col items-center text-base justify-center gap-6 max-w-[80rem] w-full"
+        class="flex flex-col sm:flex-row items-center text-base justify-between gap-6 max-w-[80rem] w-full"
       >
         <NuxtLink
           class="font-[500] italic text-lg flex items-center flex-row gap-4 tracking-wider"
           to="/"
         >
-          <BlackoutLogoType />
+          <LogoType />
+        </NuxtLink>
+
+        <NuxtLink
+          class="text-[0.5rem] sm:text-[1rem] internalFont"
+          to="/alternatives"
+        >
+          {{ $t("layout.nav.alternatives") }}
         </NuxtLink>
       </div>
     </nav>
 
     <!-- MAIN CONTENT -->
 
-    <main class="flex flex-col px-4 flex-auto items-center max-w-full">
-      <div class="flex gap-4 justify-center flex-col max-w-[60rem]">
-        <CategoryDivider>We'll be right back.</CategoryDivider>
-        <span>
-          We're experiencing technical, management and interest issues at the
-          moment. Don't worry, we'll be back REALLY SOON, but for now we need to
-          display this page instead.
-        </span>
-        <span>We apologise for any inconvenience.</span>
-        <span>- Blurry</span>
+    <main
+      class="flex flex-col flex-auto items-center mx-[5%] sm:mx-[calc(5%+2rem)] max-w-full"
+    >
+      <div class="flex justify-center flex-col max-w-[80rem]">
+        <slot />
       </div>
     </main>
 
     <!-- FOOTER -->
 
-    <BlackoutLogoScroll />
-    <div></div>
+    <LogoScroll />
+
+    <div class="flex justify-center flex-col gap-8">
+      <footer
+        class="flex flex-col gap-0 items-center pb-8 border-t border-t-slate-800 bg-[#0f0f0f]"
+      >
+        <div
+          class="flex flex-col md:flex-row items-center max-w-[80rem] md:items-start md:justify-center py-6 gap-8 mx-[calc(5%+2rem)] sm:mx-0"
+        >
+          <div class="flex flex-col items-center sm:items-start gap-4 pt-2">
+            <ColourableLogo color="var(--brand-blue)" class="h-4 w-full" />
+            <div
+              class="flex flex-col gap-1 font-[500] text-[var(--text-colour)] text-sm"
+            >
+              <NELink
+                class="font-medium text-white hover:underline"
+                to="https://github.com/notessentialsite/website"
+                external
+                >{{ $t("layout.footer.open_source") }}</NELink
+              >
+            </div>
+          </div>
+          <div class="flex flex-row gap-6 sm:gap-8">
+            <div
+              class="flex flex-col gap-2 font-[500] text-[var(--text-colour)] text-sm"
+            >
+              <span class="font-bold text-base">{{
+                $t("layout.footer.about")
+              }}</span>
+              <NELink to="/attributions"
+                ><LucideHeart class="h-5" />{{
+                  $t("layout.footer.attributions")
+                }}
+              </NELink>
+              <NELink to="https://usevital.github.io/uptime" external
+                ><LucideActivity class="h-5" />{{ $t("layout.footer.status") }}
+              </NELink>
+            </div>
+            <div
+              class="flex flex-col gap-2 font-[500] text-[var(--text-colour)] text-sm"
+            >
+              <span class="font-bold text-base">{{
+                $t("layout.footer.resources")
+              }}</span>
+              <NELink to="/contributing"
+                ><LucidePenBox class="h-5" />{{
+                  $t("layout.footer.contributing")
+                }}
+              </NELink>
+              <NELink to="/experiments"
+                ><LucideWrench class="h-5" />{{
+                  $t("layout.footer.experiments")
+                }}
+              </NELink>
+            </div>
+            <div
+              class="flex flex-col gap-2 font-[500] text-[var(--text-colour)] text-sm"
+            >
+              <span class="font-bold text-base">{{
+                $t("layout.footer.interact")
+              }}</span>
+              <NELink to="https://crowdin.com/project/notessential" external
+                ><LucideGlobe class="h-5" />Crowdin
+              </NELink>
+              <NELink to="https://discord.gg/wncdz7e8jy" external
+                ><LucideMessageCircle class="h-5" />Discord
+              </NELink>
+            </div>
+          </div>
+        </div>
+        <span class="flex flex-col gap-2 items-center">
+          <span
+            class="font-[500] text-slate-400 text-xs text-center mx-[calc(5%+2rem)] sm:mx-0"
+          >
+            {{ $t("layout.footer.disclaimer") }}
+          </span>
+        </span>
+      </footer>
+    </div>
   </div>
 </template>
 
@@ -101,15 +180,14 @@ body {
 </style>
 
 <script setup>
-useHead({
-  title: "We'll be right back.",
-  titleTemplate: "%s",
-  meta: [
-    {
-      name: "description",
-      content:
-        "We're experiencing technical, management and interest issues at the moment. Don't worry, we'll be back REALLY SOON, but for now we need to display this page instead.",
-    },
-  ],
-})
+if (process.client) {
+  const whyNot = useCookie("whyNotEssential", { default: () => false })
+  const route = useRoute()
+  const selectedExperiment = route.query.experiment || ""
+  switch (selectedExperiment) {
+    case "why":
+      whyNot.value = true
+      console.log("'Why not Essential?' experiment enabled")
+  }
+}
 </script>
