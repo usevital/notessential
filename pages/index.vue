959792c3947e5ba54c9daa5d409d45f1211dab34
Nitrrine <43351072+Nitrrine@users.noreply.github.com>
2025-04-08 09:09:34 +0500

    Minor redesign (#199)
    * Minor redesign
    
    Updated landing page, changed background
    
    * Add background to footer, make section at home page full screen, make fonts in numberedlist.vue consistent
    
    * Improve background pattern on mobile

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,38 @@
 <template>
   <div class="flex flex-col gap-8">
-    <span
-      class="text-[1.5rem] sm:text-[2rem] md:text-[3rem] lg:text-[4rem] internalFont flex flex-row flex-wrap gap-3"
-    >
-      <span class="font-bold">
-        {{ $t("landing.hero") }}
-        <span class="brand-gradient">{{ minecraftText }}</span
-        >.</span
+    <section class="flex flex-col gap-8 h-[90dvh]">
+      <span
+        class="text-[1.5rem] sm:text-[2rem] md:text-[3rem] lg:text-[4rem] internalFont flex flex-row flex-wrap gap-3"
       >
-    </span>
-    <!-- THE WORD 'MINECRAFT' AND THE 'NOTESSENTIAL' NAME SHOULD NOT BE LOCALISED. -->
+        <span class="font-bold">
+          {{ $t("landing.hero") }}
+          <span class="brand-gradient">{{ minecraftText }}</span
+          >.</span
+        >
+      </span>
+      <!-- THE WORD 'MINECRAFT' AND THE 'NOTESSENTIAL' NAME SHOULD NOT BE LOCALISED. -->
 
-    <p class="text-2xl">
-      {{ $t("landing.reason") }}
-    </p>
+      <p class="text-2xl">
+        {{ $t("landing.reason") }}
+      </p>
 
-    <NEButton class="w-fit sm:px-20 px-15 py-8" to="/alternatives">
-      <LucideArrowUpRight /> {{ $t("landing.buttons.alternatives") }}
-    </NEButton>
+      <div class="flex flex-col sm:flex-row gap-3">
+        <NEButton class="w-fit sm:px-20 px-15 py-8" to="/alternatives">
+          <LucideArrowUpRight /> {{ $t("landing.buttons.alternatives") }}
+        </NEButton>
+        <NEButton class="w-fit sm:px-20 px-15 py-8" to="#why-not?" secondary>
+          <LucideCircleHelp /> {{ $t("reasonsWhy.hero") }}
+        </NEButton>
+      </div>
+    </section>
 
     <PageHero
-      v-if="showReasons === true"
+      v-if="showReasons"
       :title="$t('reasonsWhy.hero')"
       :subTitle="$t('reasonsWhy.sub')"
     />
 
-    <NumberedList v-if="showReasons === true" :data="reasonsWhy" />
+    <NumberedList v-if="showReasons" :data="reasonsWhy" />
 
     <span>
       {{ $t("reasonsWhy.do_not_harass") }}
@@ -62,12 +69,7 @@ const minecraftText = ref("MINECRAFT")
 if (Math.floor(Math.random() * 10000) === 0) {
   minecraftText.value = "MINCERAFT"
 }
-const showReasons = ref(false)
-if (process.client) {
-  onMounted(() => {
-    showReasons.value = true
-  })
-}
+const showReasons = ref(true)
 
 const locale = useI18n().locale.value
 const altMessages = JSON.parse(
